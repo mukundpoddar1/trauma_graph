dc87180ad408c3f1e9cafd5b21be158bb9c380dc
mukundpoddar1 <mukundpoddar1@gmail.com>
2021-10-06 16:40:06 -0400

    Create adjacency list as weighted list

--- list_creator.py
+++ list_creator.py
@@ -1,28 +1,48 @@
 # %%
 import pandas as pd
 import csv
+import json
 
 # %%
 data_loc = 'RDS_ICD10_DCODE.csv'
 df = pd.read_csv(data_loc)
 # keep the first 5 digits of the DCODE
-# print out some descriptive statistics
 df['ICD10_DCODE'] = df['ICD10_DCODE'].astype(str).apply(lambda x: x[:5])
 
 
 # %%
 df['INC_KEY'] = df['INC_KEY'].astype(str)
-adj_list = df.groupby('INC_KEY')['ICD10_DCODE'].apply(list).reset_index(name='dcodes')
-
+diagnoses = df.groupby('INC_KEY')['ICD10_DCODE'].apply(list).reset_index(name='dcodes')
+diagnoses = diagnoses.dcodes
 
 # %%
-adj_list.sample(5)
+diagnoses.sample(5)
 # %%
-def write_to_file(inc_dcode, fname='adj_list.csv'):
-    to_ignore = ['[',']','"', '\'']
-    inc_dcode.dcodes = inc_dcode.dcodes.astype(str)
-    for char in to_ignore:
-        inc_dcode.dcodes = inc_dcode.dcodes.apply(lambda x: x.replace(char, ''))
-    inc_dcode.dcodes.to_csv(fname, quoting=csv.QUOTE_NONNUMERIC, header=False, index=False)
+def write_to_file(adj_list, fname='adj_list.csv'):
+    with open(fname, 'w') as file:
+        json.dump(adj_list, file)
 
+def convert_to_dict(diagnoses):
+    adj_list = {}
+    def add_diagnoses_to_list(row):
+        source = row[0]
+        for dest in row[1:]:
+            if source in adj_list:
+                if dest in adj_list[source]:
+                    adj_list[source][dest]["weight"] = adj_list[source][dest]["weight"]+1
+                else:
+                    adj_list[source]={dest:{"weight": 1}}
+            elif dest in adj_list:
+                if source in adj_list[dest]:
+                    adj_list[dest][source]["weight"] = adj_list[dest][source]["weight"]+1
+                else:
+                    adj_list[dest]={source:{"weight": 1}}
+            else:
+                adj_list[source]={dest:{"weight": 1}}
+    diagnoses.map(add_diagnoses_to_list)
+    return adj_list
+
+adj_list = convert_to_dict(diagnoses.sample(500))
 write_to_file(adj_list)
+
+#%%
