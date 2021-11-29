761c663f07cbf1752582fb302ff17ad6dd7fbd63
mukundpoddar1 <mukundpoddar1@gmail.com>
2021-11-29 11:25:20 -0500

    Create dashboard and node_finder scripts

---
+++ app.py
@@ -0,0 +1,65 @@
+import json
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import pandas as pd
+import json
+import networkx as nx
+import matplotlib.pyplot as plt
+from node_finder import connected_nodes
+
+app = dash.Dash('Trauma Dashboard')
+
+def load_nodes():
+    with open('adj_list.csv') as json_file:
+        adj_list = json.load(json_file)
+    nodes = []
+    for k, v in adj_list.items():
+        nodes.append(k)
+        for vk, _ in v.items():
+            nodes.append(vk)
+    return nodes
+
+def load_descriptions(nodes):
+    descriptions = pd.read_csv('data/RDS_ICD10_DCODEDES.csv', encoding_errors='replace')
+    descriptions['short_code'] = descriptions[descriptions['ICD10_DCODE'].apply(lambda code: type(code)==str and code!='-1' and code!='-2')]['ICD10_DCODE'].apply(lambda code: code[:5])
+    icd_desc = {node: descriptions[descriptions['short_code']==node]['ICD10_DCODEDES'].iloc[0] for node in nodes}
+    desc_icd = {v: k for k,v in icd_desc.items()}
+    return icd_desc, desc_icd
+
+injury_codes = load_nodes()
+icd_desc, desc_icd = load_descriptions(injury_codes)
+
+with open('adj_list.csv') as json_file:
+    adj_list = json.load(json_file)
+fig = plt.figure(1, figsize=(40, 40))
+net = nx.Graph(adj_list)
+
+app.layout = dbc.Container(
+    fluid=True,
+    children=[
+        dbc.Row(html.H1('Trauma Dashboard'), align="center"),
+        dbc.Form(
+        [
+            dbc.Label('Choose injuries:'),
+            dcc.Dropdown( id='injuries',
+                options=[{'label': desc, 'value': code} for code, desc in icd_desc.items()],
+                multi=True)
+            ]),
+            dcc.Checklist(id='predicted', labelStyle = dict(display='block'))
+        ]
+    )
+
+@app.callback(
+    Output('predicted', 'options'), Input('injuries', 'value')
+)
+def get_predictions(input_injuries):
+    related = connected_nodes(net, input_injuries[0], 2, 0, 0.0000001)[0]
+    print(related)
+    results = []
+    [results.extend(x) for x in related.values()]
+    return [{'label': icd_desc[node], 'value': node} for node in results]
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ node_finder.py
@@ -0,0 +1,69 @@
+import networkx as nx
+
+def connected_nodes(graph, source_node, degree, number, weight_thres):
+    '''
+    this function is to 
+    - print out all of the nodes associated to the source node corresponding the number of connected edges
+    - plot the subgraph from the overall graph containing the source_node
+
+    input: 
+    graph: networkx graph
+    source_node: source node name
+    degree: number of edges in between the source_node and the returned node
+    number: number of nodes to be returned
+
+    output: nodes which satisfy requirments as specified above
+
+    note: essentially the idea is to extract a subgraph coming from that node
+    the result of this could be further verified by using nx.descendants(graph, source_node)
+    '''
+    temp_graph = nx.Graph()
+    result_nodes = {}
+    all_nodes = [source_node]
+    neighbors = [x for x in graph.neighbors(source_node)]
+    weights = {}
+    cur_num_edge = 1
+
+    # iterate through current node in neighbor of the source node:
+
+    # update node
+    for cur_node in neighbors:
+        weights[cur_node] = graph.get_edge_data(source_node,cur_node)['weight']
+    # update result_nodes at number of edge = 1
+    result_nodes[cur_num_edge] = [x for x in neighbors if weights[x]>weight_thres]
+    # updates all node (all of the node which we have previously go through)
+    all_nodes.extend(neighbors)
+    # update neighbors information (which satistfy the weight threshold)
+    neighbors = [x for x in neighbors if weights[x] > weight_thres]
+    # iterate through the neighbors node, add edge to the output graph
+    for ii in neighbors:
+        temp_graph.add_edge(source_node, ii)
+
+    # iterate through the neighbors of the neighbors
+    while len(neighbors) != 0 and cur_num_edge < degree:
+
+        temp = []
+
+        cur_num_edge += 1
+
+        for node in neighbors:
+
+            temp.extend([x for x in graph.neighbors(node) if x not in all_nodes])
+
+            cur_weight = weights[node]
+            for cur_node in graph.neighbors(node):
+                if cur_node not in all_nodes:
+                    weights[cur_node] = cur_weight * graph.get_edge_data(node, cur_node)['weight']
+                    if weights[cur_node] > weight_thres:
+                        temp_graph.add_edge(node, cur_node)
+            all_nodes.extend(temp)
+        if len(temp)!=0:
+            result_nodes[cur_num_edge] = [x for x in temp if weights[x]>weight_thres]
+        neighbors = [x for x in temp if weights[x]>weight_thres]
+    
+
+    #nx.draw(graph.subgraph(nx.shortest_path(graph.to_undirected(),source_node)),with_labels = True)
+    
+
+    return result_nodes, weights, temp_graph
+
